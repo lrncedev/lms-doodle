59c2281779ae5912624143cee58dc61f031205b1
Laurence Cadag <laurencecads1@gmail.com>
2022-12-18 00:06:09 +0800

    includes folder for html boilerplate

---
+++ includes/bottom.php
@@ -0,0 +1,7 @@
+<!-- <script src="https://unpkg.com/flowbite@1.5.5/dist/flowbite.js"></script> -->
+
+<!-- Font awesome Kit-->
+<script src="https://kit.fontawesome.com/66e98214bd.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ includes/top.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.5/dist/flowbite.min.css" /> -->
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
